439952fd3067edd5a6ccc956b4b24fea0ae67987
Leonardo Suarez Restrepo <leonardosuarez16@hotmail.com>
2022-10-27 22:12:03 -0500

    proyecto ciclo 4 backendmascotas sprint 2

---
+++ src/models/index.ts
@@ -0,0 +1,6 @@
+export * from './prospecto.model';
+export * from './sucursal.model';
+export * from './productos-servicio.model';
+export * from './mascota.model';
+export * from './usuario.model';
+export * from './plan.model';
