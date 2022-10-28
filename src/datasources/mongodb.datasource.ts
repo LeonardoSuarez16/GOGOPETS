05c6957d193d65f45605cda4fa9b5043f33861a9
leonardo davinci suarez restrepo <111334009+LeonardoSuarez16@users.noreply.github.com>
2022-10-28 13:31:10 -0500

    Update mongodb.datasource.ts

--- src/datasources/mongodb.datasource.ts
+++ src/datasources/mongodb.datasource.ts
@@ -4,7 +4,7 @@ import {juggler} from '@loopback/repository';
 const config = {
   name: 'mongodb',
   connector: 'mongodb',
-  url: 'mongodb+srv://LeoAutosleo:caro1234@clusterautosleo.fnpvuek.mongodb.net/MascotasDB?retryWrites=true&w=majority',
+  url: 'mongodb+srv://LeoAutosleo:@clusterautosleo.fnpvuek.mongodb.net/MascotasDB?retryWrites=true&w=majority',
   host: '',
   port: 0,
   user: '',
